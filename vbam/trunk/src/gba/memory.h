14618f1b5fc553eefced90ce42aeb7342d4873b9
spacy51 <spacy51@5a53c671-dd2d-0410-9261-3f5c817b7aa0>
2009-09-14 22:20:02 +0000

    forgot this

---
+++ vbam/trunk/src/gba/memory.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "../common/Types.h"
+
+#define CPUReadByteQuick(addr)  map[(addr)>>24].address[(addr) & map[(addr)>>24].mask]
+#define CPUReadHalfWordQuick(addr)  READ16LE(((u16*)&map[(addr)>>24].address[(addr) & map[(addr)>>24].mask]))
+#define CPUReadMemoryQuick(addr)  READ32LE(((u32*)&map[(addr)>>24].address[(addr) & map[(addr)>>24].mask]))
+
+u8 CPUReadByte(u32 address);
+u32 CPUReadHalfWord(u32 address);
+u16 CPUReadHalfWordSigned(u32 address);
+void CPUWriteMemory(u32 address, u32 value);
+void CPUWriteHalfWord(u32 address, u16 value);
+void CPUWriteByte(u32 address, u8 b);
+u32 CPUReadMemory(u32 address);
