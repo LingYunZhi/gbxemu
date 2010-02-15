a304a027d63910d7100b7aef034572bb5fb04c45
spacy51 <spacy51@5a53c671-dd2d-0410-9261-3f5c817b7aa0>
2010-02-15 14:41:18 +0000

    add more BITxx constants

--- vba2/gba2/common/Types.h
+++ vba2/gba2/common/Types.h
@@ -64,5 +64,22 @@
 #define BIT14  0x4000
 #define BIT15  0x8000
 
+#define BIT16  0x10000
+#define BIT17  0x20000
+#define BIT18  0x40000
+#define BIT19  0x80000
+#define BIT20  0x100000
+#define BIT21  0x200000
+#define BIT22  0x400000
+#define BIT23  0x800000
+#define BIT24  0x1000000
+#define BIT25  0x2000000
+#define BIT26  0x4000000
+#define BIT27  0x8000000
+#define BIT28  0x10000000
+#define BIT29  0x20000000
+#define BIT30  0x40000000
+#define BIT31  0x80000000
+
 
 #endif // TYPES_H
